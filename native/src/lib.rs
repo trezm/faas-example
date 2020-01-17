d6ef99ceadd444423a3a8d6a3df422b2d0df5bb3
Pete Mertz <peter.s.mertz@gmail.com>
2020-01-16 21:44:10 -0500

    Final version

--- native/src/lib.rs
+++ native/src/lib.rs
@@ -1,13 +1,13 @@
 use neon::register_module;
 use neon::prelude::*;
+use thruster::hyper_server::HyperServer;
 use thruster::thruster_context::basic_hyper_context::{
-    generate_context, BasicHyperContext as Ctx,
+    generate_context, BasicHyperContext as Ctx, HyperRequest,
 };
+use thruster::thruster_proc::{async_middleware, middleware_fn};
+use thruster::{App, ThrusterServer};
 use thruster::{MiddlewareNext, MiddlewareReturnValue, MiddlewareResult};
-use thruster::thruster_proc::{middleware_fn};
-use thruster_x::{hyper_express as express};
 
-#[express(generate_context)]
 #[middleware_fn]
 async fn fib(mut context: Ctx, _next: MiddlewareNext<Ctx>) -> MiddlewareResult<Ctx> {
     let n = context.params.get("n").unwrap().parse::<u128>().unwrap();
@@ -28,8 +28,20 @@ fn fibonacci(number: u128) -> u128 {
     b
 }
 
+fn start(mut cx: FunctionContext) -> JsResult<JsNull> {
+    let port = cx.argument::<JsNumber>(0)?.value();
+
+    let mut app = App::<HyperRequest, Ctx>::create(generate_context);
+    app.get("/v2/fib/:n", async_middleware!(Ctx, [fib]));
+
+    let server = HyperServer::new(app);
+    server.start("0.0.0.0", port as u16);
+
+    Ok(cx.null())
+}
+
 register_module!(mut cx, {
-    let _ = cx.export_function("fib", express_fib);
+    let _ = cx.export_function("start", start);
 
     Ok(())
 });
